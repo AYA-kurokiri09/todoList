3eaf0f932eb9e7d2eab11635945cdaa41c6f52b6
AYA-kurokiri09 <60229621+AYA-kurokiri09@users.noreply.github.com>
2020-06-27 16:58:44 +0900

    implement task adding

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Request;
+use App\Content;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class TodoController extends Controller
 {
-    public function index() {
-        return view('todo.index');
+    public function index(Request $request) {
+        $items = Content::all();
+        return view('todo.index', ['items' => $items], ['input' => '']);
+    }
+
+    public function add(Request $request) {
+        $content = new Content();
+        $content->fill([
+            'comment' => $request->input,
+            'condition' => '作業中',])
+            ->save();
+
+        $items = Content::all();
+        return view('todo.index', ['items' => $items], ['input' => '']);
     }
 }
